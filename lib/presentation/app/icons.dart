185b14892f25153a373da4aaebf7f300e7a5fb54
Radomir Epur <radomir9720@gmail.com>
2022-09-07 16:29:09 +0300

    feat: added icons

---
+++ lib/presentation/app/icons.dart
@@ -0,0 +1,40 @@
+// Place fonts/PixelIcons.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: PixelIcons
+//      fonts:
+//       - asset: fonts/PixelIcons.ttf
+import 'package:flutter/widgets.dart';
+
+class PixelIcons {
+  PixelIcons._();
+
+  static const String _fontFamily = 'PixelIcons';
+
+  static const IconData wifi = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData bluetooth = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData usb = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData info = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData back = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData close = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData charging = IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData apps = IconData(0xe907, fontFamily: _fontFamily);
+  static const IconData navigator = IconData(0xe908, fontFamily: _fontFamily);
+  static const IconData car = IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData about = IconData(0xe90a, fontFamily: _fontFamily);
+  static const IconData licenses = IconData(0xe90b, fontFamily: _fontFamily);
+  static const IconData fastActions = IconData(0xe90c, fontFamily: _fontFamily);
+  static const IconData power = IconData(0xe90d, fontFamily: _fontFamily);
+  static const IconData windshieldBlower = IconData(0xe90e, fontFamily: _fontFamily);
+  static const IconData airflow = IconData(0xe90f, fontFamily: _fontFamily);
+  static const IconData fan = IconData(0xe910, fontFamily: _fontFamily);
+  static const IconData belt = IconData(0xe911, fontFamily: _fontFamily);
+  static const IconData settings = IconData(0xe912, fontFamily: _fontFamily);
+  static const IconData unlocked = IconData(0xe913, fontFamily: _fontFamily);
+  static const IconData locked = IconData(0xe914, fontFamily: _fontFamily);
+  static const IconData light = IconData(0xe915, fontFamily: _fontFamily);
+  static const IconData speedometer = IconData(0xe916, fontFamily: _fontFamily);
+  static const IconData sunCharging = IconData(0xe917, fontFamily: _fontFamily);
+  static const IconData battery = IconData(0xe918, fontFamily: _fontFamily);
+}
