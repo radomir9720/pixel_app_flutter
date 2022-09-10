3c79416d0510ab90d2d09458530e95bbc36fecc0
Radomir Epur <radomir9720@gmail.com>
2022-09-10 08:37:49 +0300

    fix: status bar brightness

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:pixel_app_flutter/l10n/l10n.dart';
 import 'package:pixel_app_flutter/presentation/app/colors.dart';
@@ -38,7 +39,12 @@ class _AppState extends State<App> {
                 data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
                 child: AppTypography(
                   child: Screen(
-                    child: child ?? const SizedBox.shrink(),
+                    child: AnnotatedRegion<SystemUiOverlayStyle>(
+                      value: SystemUiOverlayStyle(
+                        statusBarBrightness: Theme.of(context).brightness,
+                      ),
+                      child: child ?? const SizedBox.shrink(),
+                    ),
                   ),
                 ),
               );
