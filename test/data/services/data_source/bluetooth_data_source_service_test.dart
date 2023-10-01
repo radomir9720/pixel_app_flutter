d7dc9c258112913614f45ef7880ed30c778392af
Radomir Epur <radomir9720@gmail.com>
2023-10-01 21:49:32 +0300

    test: change battery power parameter id

--- test/data/services/data_source/bluetooth_data_source_service_test.dart
+++ test/data/services/data_source/bluetooth_data_source_service_test.dart
@@ -279,7 +279,7 @@ void main() {
             'when in the buffer remained more than or equal '
             'to minimum package length', () async {
           // arrange
-          const validPackage = [60, 0, 149, 84, 0, 3, 97, 124, 6, 56, 169, 62];
+          const validPackage = [60, 0, 149, 87, 0, 3, 97, 124, 6, 69, 165, 62];
           ds.buffer.addAll([
             1, 2, 3, 4, 5, 6, //
             ...validPackage,
